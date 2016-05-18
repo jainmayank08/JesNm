f54a2a8006f3ccccf918d4118fa417881c0eee90
Mayank <jainmayank08@gmail.com>
2016-05-18 10:23:42 +0530

    added chapter

---
+++ JesNm.Web/Controllers/ChapterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JesNm.Web.Controllers
+{
+    public class ChapterController : Controller
+    {
+        // GET: Chapter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
